e1a7bb6ed8ad63e4b2246bd9717676c1fef45dd8
mihyunLee <mihy621@gmail.com>
2023-01-03 14:42:53 +0900

    05-7. 인터페이스와 클래스

--- udemy/basic/05/src/app.ts
+++ udemy/basic/05/src/app.ts
@@ -1,130 +1,50 @@
-class Department {
-  /* class field */
-  // protected: private와 비슷하지만 이 클래스 뿐만 아니라
-  // 확장하는 모든 클래스에서도 사용 가능한 속성으로 만들어준다.
-  protected employees: string[] = [];
+// (1) 인터페이스는 초기자를 가질 수 없다.
+// 객체의 구조를 정의한다.
+interface Greetable {
+  name: string;
 
-  static createYear = 2022; // 정적 속성
+  greet(phrase: string): void;
+}
 
-  // 클래스 필드를 축약해서 생성할 수 있다.
-  constructor(private readonly id: string, public name: string) {}
+// (2) 사용자 정의 타입(type)과는 다르게 선언을 병합할 수 있다.
+interface Greetable {
+  sayHi(): void;
+}
 
-  // 정적 메서드
-  static createEmployee(name: string) {
-    return { name: name };
+// (3) 인터페이스 구현
+// 상속과는 다르게 여러개의 인터페이스를 하나의 클래스에 구현할 수 있다.
+// ex) class Person implements Greetable, AnotherInterface {...}
+// 클래스는 인터페이스에 정의된 내용을 무조건 포함해야 한다.
+class Person implements Greetable {
+  constructor(public name: string, public age: number) {}
+
+  greet(phrase: string): void {
+    console.log(phrase + " " + this.name);
   }
 
-  /* method */
-  describe(this: Department) {
-    // 클래스 내부에서 정적 속성이나 메서드에 접근하려면
-    // this 키워드가 아닌 해당하는 클래스 명을 사용해야 한다.
-    console.log(
-      `Department (${this.id}): ${Department.createYear} ${this.name}`
-    );
-  }
-
-  addEmployee(employee: string) {
-    this.employees.push(employee);
-  }
-
-  printEmployeeInformation() {
-    console.log(this.employees.length);
-    console.log(this.employees);
+  sayHi(): void {
+    console.log("Hi, " + this.name);
   }
 }
 
-// extends: 상속 키워드
-class ITDepartment extends Department {
-  constructor(id: string, public admins: string[]) {
-    // 상속받은 클래스의 속성값 외에 특정 속성을 추가하기 위해서는
-    // super를 호출해야한다.
-    super(id, "IT");
-  }
+// 인터페이스로 객체 생성
+let user1: Greetable;
 
-  printAdmins() {
-    console.log(this.admins);
-  }
-}
+user1 = {
+  name: "Max",
+  greet(phrase: string) {
+    console.log(phrase + " " + this.name);
+  },
+  sayHi() {
+    console.log("Hi, " + this.name);
+  },
+};
 
-class AccountingDepartment extends Department {
-  private lastReport: string;
-  private static instance: AccountingDepartment;
+user1.greet("hi there - I am "); // hi there - I am Max
+user1.sayHi(); // Hi, Max
 
-  // getter
-  get mostRecentReport() {
-    if (this.lastReport) {
-      return this.lastReport;
-    }
+// 클래스로 인스턴스 생성
+let user2 = new Person("Anna", 30);
 
-    throw new Error("No report found");
-  }
-
-  // setter
-  set mostRecentReport(value: string) {
-    if (!value) {
-      throw new Error("Please pass in valid value!");
-    }
-    this.addReport(value);
-  }
-
-  // private 생성자로 만들면 new 클래스 구문 사용 불가
-  private constructor(id: string, private reports: string[]) {
-    super(id, "Accounting");
-    this.lastReport = reports[0];
-  }
-
-  // 단 하나의 인스턴스만 생성하도록 하는 정적 메서드 선언
-  // 싱글톤 패턴을 구현할 수 있다.
-  static getInstance() {
-    if (!AccountingDepartment.instance) {
-      this.instance = new AccountingDepartment("d1", ["Report1", "Report2"]);
-    }
-    return this.instance;
-  }
-
-  // 기존 메소드 재정의
-  addEmployee(name: string) {
-    if (name === "Max") {
-      return;
-    }
-    this.employees.push(name);
-  }
-
-  addReport(text: string) {
-    this.reports.push(text);
-    this.lastReport = text;
-  }
-
-  printReports() {
-    console.log(this.reports);
-  }
-}
-
-const employee1 = Department.createEmployee("Max");
-console.log(employee1, Department.createYear);
-
-// new 클래스 구문 사용 불가
-// const accounting = new AccountingDepartment("d1", ["Report1", "Report2"]);
-const accounting = AccountingDepartment.getInstance();
-const accounting2 = AccountingDepartment.getInstance();
-
-console.log(accounting === accounting2); // true, 동일한 단 하나의 인스턴스만 생성됨(싱글톤 패턴)
-
-accounting.addEmployee("Max"); // 추가되지 않음
-accounting.addEmployee("Manu");
-
-// accounting.employees[2] = "Anna"; // Error: 'employees' property is private
-
-accounting.describe();
-
-accounting.addReport("Additional Report");
-accounting.mostRecentReport = "Recent Report";
-console.log(accounting.mostRecentReport);
-
-accounting.printReports();
-accounting.printEmployeeInformation();
-
-const accountingIT = new ITDepartment("d2", ["Max"]);
-
-accountingIT.describe();
-accountingIT.printAdmins();
+user2.greet("hi there - I am "); // hi there - I am Anna
+user2.sayHi(); // Hi, Anna
---
+++ udemy/basic/05/src/class.ts
@@ -0,0 +1,130 @@
+class Department {
+  /* class field */
+  // protected: private와 비슷하지만 이 클래스 뿐만 아니라
+  // 확장하는 모든 클래스에서도 사용 가능한 속성으로 만들어준다.
+  protected employees: string[] = [];
+
+  static createYear = 2022; // 정적 속성
+
+  // 클래스 필드를 축약해서 생성할 수 있다.
+  constructor(private readonly id: string, public name: string) {}
+
+  // 정적 메서드
+  static createEmployee(name: string) {
+    return { name: name };
+  }
+
+  /* method */
+  describe(this: Department) {
+    // 클래스 내부에서 정적 속성이나 메서드에 접근하려면
+    // this 키워드가 아닌 해당하는 클래스 명을 사용해야 한다.
+    console.log(
+      `Department (${this.id}): ${Department.createYear} ${this.name}`
+    );
+  }
+
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
+}
+
+// extends: 상속 키워드
+class ITDepartment extends Department {
+  constructor(id: string, public admins: string[]) {
+    // 상속받은 클래스의 속성값 외에 특정 속성을 추가하기 위해서는
+    // super를 호출해야한다.
+    super(id, "IT");
+  }
+
+  printAdmins() {
+    console.log(this.admins);
+  }
+}
+
+class AccountingDepartment extends Department {
+  private lastReport: string;
+  private static instance: AccountingDepartment;
+
+  // getter
+  get mostRecentReport() {
+    if (this.lastReport) {
+      return this.lastReport;
+    }
+
+    throw new Error("No report found");
+  }
+
+  // setter
+  set mostRecentReport(value: string) {
+    if (!value) {
+      throw new Error("Please pass in valid value!");
+    }
+    this.addReport(value);
+  }
+
+  // private 생성자로 만들면 new 클래스 구문 사용 불가
+  private constructor(id: string, private reports: string[]) {
+    super(id, "Accounting");
+    this.lastReport = reports[0];
+  }
+
+  // 단 하나의 인스턴스만 생성하도록 하는 정적 메서드 선언
+  // 싱글톤 패턴을 구현할 수 있다.
+  static getInstance() {
+    if (!AccountingDepartment.instance) {
+      this.instance = new AccountingDepartment("d1", ["Report1", "Report2"]);
+    }
+    return this.instance;
+  }
+
+  // 기존 메소드 재정의
+  addEmployee(name: string) {
+    if (name === "Max") {
+      return;
+    }
+    this.employees.push(name);
+  }
+
+  addReport(text: string) {
+    this.reports.push(text);
+    this.lastReport = text;
+  }
+
+  printReports() {
+    console.log(this.reports);
+  }
+}
+
+const employee1 = Department.createEmployee("Max");
+console.log(employee1, Department.createYear);
+
+// new 클래스 구문 사용 불가
+// const accounting = new AccountingDepartment("d1", ["Report1", "Report2"]);
+const accounting = AccountingDepartment.getInstance();
+const accounting2 = AccountingDepartment.getInstance();
+
+console.log(accounting === accounting2); // true, 동일한 단 하나의 인스턴스만 생성됨(싱글톤 패턴)
+
+accounting.addEmployee("Max"); // 추가되지 않음
+accounting.addEmployee("Manu");
+
+// accounting.employees[2] = "Anna"; // Error: 'employees' property is private
+
+accounting.describe();
+
+accounting.addReport("Additional Report");
+accounting.mostRecentReport = "Recent Report";
+console.log(accounting.mostRecentReport);
+
+accounting.printReports();
+accounting.printEmployeeInformation();
+
+const accountingIT = new ITDepartment("d2", ["Max"]);
+
+accountingIT.describe();
+accountingIT.printAdmins();
