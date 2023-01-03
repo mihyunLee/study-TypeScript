973d8ddc787254f11550416fcf78627587875bde
mihyunLee <mihy621@gmail.com>
2023-01-03 16:14:42 +0900

    05-10. 인터페이스와 함수타입, 옵셔널 설정

--- udemy/basic/05/src/app.ts
+++ udemy/basic/05/src/app.ts
@@ -1,5 +1,20 @@
+// 함수 타입
+// type AddFn = (a: number, b: number) => number;
+
+// 인터페이스는 함수 타입도 정의할 수 있다.
+interface AddFn {
+  (a: number, b: number): number;
+}
+
+let add: AddFn;
+
+add = (n1: number, n2: number) => {
+  return n1 + n2;
+};
+
 interface Named {
-  readonly name: string;
+  readonly name?: string;
+  outputName?: string; // '?'를 사용하여 옵셔널 속성을 만들 수 있다.
 }
 
 // (1) 인터페이스는 초기자를 가질 수 없다.
@@ -21,14 +36,22 @@ interface Greetable {
 // ex) class Person implements Greetable, AnotherInterface {...}
 // 클래스는 인터페이스에 정의된 내용을 무조건 포함해야 한다.
 class Person implements Greetable {
-  constructor(public name: string, public age: number) {}
+  // '?'를 사용해서 optional 매개변수로 만들거나, 디폴트 값을 주게 되면
+  // 인스턴스 생성시에 인자에 값을 전달하지 않아도 되는 공통점이 있다.
+  constructor(public name?: string, public age = 30) {}
 
   greet(phrase: string): void {
-    console.log(phrase + " " + this.name);
+    if (this.name) {
+      console.log(phrase + " " + this.name);
+    } else {
+      console.log("Who are you?");
+    }
   }
 
   sayHi(): void {
-    console.log("Hi, " + this.name);
+    if (this.name) {
+      console.log("Hi, " + this.name);
+    }
   }
 }
 
@@ -53,8 +76,8 @@ user1.sayHi(); // Hi, Max
 // 클래스로 인스턴스 생성
 // 인터페이스가 구현된 클래스에 read-only가 적용되려면
 // 클래스에 인터페이스 타입을 지정해주어야 한다.
-let user2: Greetable = new Person("Anna", 30);
+let user2: Greetable = new Person();
 // user2.name = "Max"; // ERROR! name is read-only property
 
-user2.greet("hi there - I am "); // hi there - I am Anna
-user2.sayHi(); // Hi, Anna
+user2.greet("hi there - I am "); // Who are you?
+user2.sayHi(); // 출력되지 X.
