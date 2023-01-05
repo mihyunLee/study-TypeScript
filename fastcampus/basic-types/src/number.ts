f9a6b7ba536e89a33781b1b9bfd35c6f8a6f4b01
Nor <51310674+mihyunLee@users.noreply.github.com>
2023-01-05 11:05:57 +0900

    Basic Types - 02. number

---
+++ fastcampus/basic-types/src/number.ts
@@ -0,0 +1,8 @@
+let decimal: number = 6;
+let hex: number = 0xf00d;
+let binary: number = 0b1010;
+let octal: number = 0o744;
+
+let notANumber: number = NaN; // number 타입에 NaN도 할당할 수 있다.
+
+let underScoreNum: number = 1_000_000; // 밑줄도 할당 가능!
