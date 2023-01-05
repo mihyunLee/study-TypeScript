df598364d90e3a7dae37679ff00bc4e8ff0751a6
Nor <51310674+mihyunLee@users.noreply.github.com>
2023-01-05 11:10:45 +0900

    Basic Types - 03. string

---
+++ fastcampus/basic-types/src/string.ts
@@ -0,0 +1,10 @@
+let myName: string = "Mark Lee";
+let age: number = 39;
+
+// 리터럴 표기법도 사용 가능
+let sentence: string = `Hi, my name is ${myName}.
+I am ${age} years old.`;
+// Hi, my name is Mark Lee.
+// I am 39 years old.
+
+console.log(sentence);
