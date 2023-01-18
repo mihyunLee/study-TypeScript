6793075b17aec9209d6694e4538197dfdf69060f
Nor <51310674+mihyunLee@users.noreply.github.com>
2023-01-18 22:18:27 +0900

    Generic - 04. Generics Function

---
+++ fastcampus/generic/generic4.ts
@@ -0,0 +1,15 @@
+// Type Alias
+type HelloFunctionGeneric1 = <T>(message: T) => T;
+
+const helloFunction1: HelloFunctionGeneric1 = <T>(message: T): T => {
+  return message;
+};
+
+// Interface
+interface HelloFunctionGeneric2 {
+  <T>(message: T): T;
+}
+
+const helloFunction2: HelloFunctionGeneric2 = <T>(message: T): T => {
+  return message;
+};
